9eb99d5c0c67e02d631d47596183957c05137d8f
Allan Mujera <allanmujera91@gmail.com>
2026-06-17 16:35:05 +0300

    Fix PORT definition and remove extra semicolons

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,7 @@ const crypto = require('crypto');
 const path = require('path');
 
 const app = express();
+const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
@@ -356,7 +357,8 @@ dbReady.then(() => {
 }).catch(err => {
   console.error('Failed to connect to database:', err);
   process.exit(1);
-}););
+});;
+
 
 
 
